225e34d27d3766d47c117ce5fd27fcf892c4bb0d
TeamSibes <teamsibes@gmail.com>
2017-09-12 20:36:19 -0600

    cambios paola

--- resources/views/publico/avista_sub.blade.php
+++ resources/views/publico/avista_sub.blade.php
@@ -1,20 +1,16 @@
 
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>MARN | SIBES</title>
     <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel=stylesheet href="css/estilo_menu.css" type="text/css">
-<link rel=stylesheet href="css/estilo_busqueda.css" type="text/css">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<link rel="shortcut icon" type="image/ico" href="/imagen/favicon.ico" />
-<link rel="stylesheet" type="text/css" href="css/side_bar_nav.css">
-
-
-
-
+    <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel=stylesheet href="css/estilo_menu.css" type="text/css">
+    <link rel=stylesheet href="css/estilo_busqueda.css" type="text/css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <link rel="shortcut icon" type="image/ico" href="/imagen/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/side_bar_nav.css">
 
 <style type="text/css">
 
@@ -23,7 +19,7 @@
     }
 
     @media screen and (min-width: 1000px) {
-    
+
 
     .cuerpo{
       height:600px;
@@ -51,7 +47,7 @@
 </style><style>
 body  {
     background-image: url("/imagen/patron2.png");
-   
+
 </style>
 
 
@@ -64,7 +60,7 @@ body  {
     </header>
 
 
-    <div class="row">
+    <div class="row" id="publico_sibes">
 
         <div class="col-sm-12 col-md-2 side_BAR " >
 
@@ -81,12 +77,12 @@ body  {
         </div>
         <div class="col-md-12" >
         <div class="contenido" >
-         
+
 
         @foreach( $especie as $esp )
 
             <form id="frm-desc-avista" action="Exportar_excel_sub" method="get" >
-                
+
                 <input type="hidden" name="id_sub" value="{{ $esp->idSubespecie }}" >
                 <input type="hidden" name="nom_sub" value="{{ $esp->nombreSubespecie }}" >
 
@@ -125,14 +121,14 @@ body  {
                 <div class="row" ><label> Nombre Comun :  </label>
 
                     @foreach(  $nc_sub as $nc )
-                        
+
                         {{ $nc->nombreComun }}
                         @if($loop->last)
                         @else,
                         @endif
 
                     @endforeach
-              
+
                 </div>
 
 
@@ -152,7 +148,7 @@ body  {
                 <div class="row"><label>Apendice CITES : </label> {{ $esp->nombreApendiceCITES }} </div>
                 <div class="row"> <label>No. Avistamientos : </label> {{$no_avis}} </div>
                 <br>
-                
+
 
                 </div>
 
@@ -162,14 +158,14 @@ body  {
                 </div>
                 <div class="row"  style="padding-left: 40px;padding-right: 40px;" >
 
-                    <label> Descripccion del Ejemplar : </label><p>{{ $esp->descripcionDelEjemplar }}</p><br>                   
+                    <label> Descripccion del Ejemplar : </label><p>{{ $esp->descripcionDelEjemplar }}</p><br>
 
                 </div>
 
             </div>
-            
 
-            
+
+
 
         </div>
 
@@ -179,7 +175,7 @@ body  {
             <center>
             <img src="/imagen_especie/{{$esp->nombreEspecie}}/{{$esp->nombreSubespecie}}/{{$esp->fotografiaEspecie2}}"  id="img-avista" class="img-rounded" width="380" height="330" >
             <br><br>
-            <label class="btn btn-success" id="desc_avista" style="width:200px;background-color: #b0a54f ;border-color: #8e7200 ; " >  Descargar <span class="glyphicon glyphicon-save" aria-hidden="true"  ></span> 
+            <label class="btn btn-success" id="desc_avista" style="width:200px;background-color: #b0a54f ;border-color: #8e7200 ; " >  Descargar <span class="glyphicon glyphicon-save" aria-hidden="true"  ></span>
             </center>
             </div>
         @else
@@ -188,15 +184,15 @@ body  {
             <center>
             <img src="/imagen/placeholder.png"  id="img-avista" class="img-rounded" width="380" height="330" >
             <br><br>
-            <label class="btn btn-success" id="desc_avista" style="width:200px;background-color: #b0a54f ;border-color: #8e7200 ; " >  Descargar <span class="glyphicon glyphicon-save" aria-hidden="true"  ></span> 
-            </center>            
+            <label class="btn btn-success" id="desc_avista" style="width:200px;background-color: #b0a54f ;border-color: #8e7200 ; " >  Descargar <span class="glyphicon glyphicon-save" aria-hidden="true"  ></span>
+            </center>
             </div>
         @endif
 
         </div>
 
         @endforeach
-        
+
         <h5 style="color: #54a049;" ><b>Lista de avistamientos de la subespecie {{$esp->nombreSubespecie}}, {{$esp->nombreEspecie}} en El Salvador</b> </h5>
 
         <div class="container-fluid panel" style="width:95%;float:left;" >
@@ -211,7 +207,7 @@ body  {
 
     </div>
     </div>
-        
+
 
     <script src="js/jquery-3.2.1.min.js"> </script>
     <script src="js/bootstrap.min.js"> </script>
