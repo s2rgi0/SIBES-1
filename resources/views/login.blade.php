230bb72677712dc60e245e129403592fe56e38b6
TeamSibes <teamsibes@gmail.com>
2017-09-12 20:02:25 -0600

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,24 +1,34 @@
 <!DOCTYPE doctype html>
-<html lang="es">
 <head>
         <meta charset="utf-8">
         <title>MARN | SIBES</title>
         <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel=stylesheet href="css/estilo_busqueda.css" type="text/css">
+        <link rel="shortcut icon" type="image/ico" href="/imagen/favicon.ico" />
         <script language="JavaScript">
-          function nobackbutton(){
-           window.location.hash="no-back-button";
-           window.location.hash="Again-No-back-button" //chrome
-           window.onhashchange=function(){window.location.hash="no-back-button";}
-        }
-        </script>
+  function nobackbutton(){
+   window.location.hash="no-back-button";
+   window.location.hash="Again-No-back-button" //chrome
+   window.onhashchange=function(){window.location.hash="no-back-button";}
+}
+</script>
+<style>
+body{
+    background-image: url("/imagen/patron2.png");
+  }
+
+  nav{
+    box-shadow: 0 7px 10px 0 rgba(0, 0, 0, 0.2);
+  }
+</style>
 </head>
 <body onload="nobackbutton();">
     <header>
       @include('parciales.nav')
     </header>
 
+  @include('parciales.barra')
 <div class="contanier">
 
 <br><br><br><br>
@@ -26,33 +36,33 @@
 <center>
  <form class="form-horizontal"   style="width: 500px;" action="auth_usuario" method="get" >
   <div class="form-group">
-    <label for="nombre_usr" class="col-sm-2 control-label"  >Usuario</label>
+    <label for="inputEmail3" class="col-sm-2 control-label"  >Usuario</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" name="nombre_usr" placeholder="nombre usuario">
     </div>
   </div>
 
   <div class="form-group">
-    <label for="ntento_usr" class="col-sm-2 control-label"  >Contraseña</label>
+    <label for="inputPassword3" class="col-sm-2 control-label"  >Contraseña</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" name="intento_usr" placeholder="contraseña">
     </div>
     <br>
     <br>
     @if( count($msg)== 1 )
-      <label style="color: #ff3700 ;" > <strong>  {{$msg}}  </strong> </label>
+      <center><h5 style="color: #ff3700 ;" > {{$msg}}  </h5></center>
     @endif
   </div>
 
-
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-success btn-guardar btn-md"  style="width: 200px;background-color: : orange;" >Ingresar </button>
+      <button type="submit" class="btn btn-default btn-guardar btn-md"  style="width: 200px;" >Ingresar </button>
 
     </div>
   </div>
 
 </form>
+
 
 
 </center>
